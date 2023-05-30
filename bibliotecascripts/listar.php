97dc04a66f1e8a34f64899f695e7cc0cc77852f3
Marcelo <marccelo.moro@gmail.com>
2023-05-30 17:26:54 -0300

    tentativa 1

--- bibliotecascripts/listar.php
+++ bibliotecascripts/listar.php
@@ -3,28 +3,32 @@
 include("conexao.php");
 
 
-$sql = "SELECT * from livros";
-$resultado = $conexao->query($sql);
-print("pegou");
+function criaJSON($sql){
+    $resultado = $conexao->query($sql);
+    if($resultado->num_rows > 0){
+        $rows = array();
+   
+       // Itera sobre os resultados e adiciona cada linha ao array
+       while ($row = $resultado->fetch_assoc()) {
+           $rows[] = $row;
+       }
+   
+       // Converte o array em JSON
+       $json = json_encode($rows);
+   
+       // Exibe o JSON  
+       $json_com_quebra_de_linha = nl2br($json);
+   
+       echo $json_com_quebra_de_linha;
 
-if($resultado->num_rows > 0){
-     $rows = array();
-
-    // Itera sobre os resultados e adiciona cada linha ao array
-    while ($row = $resultado->fetch_assoc()) {
-        $rows[] = $row;
     }
-
-    // Converte o array em JSON
-    $json = json_encode($rows);
-
-    // Exibe o JSON  
-    $json_com_quebra_de_linha = nl2br($json);
-
-    echo $json_com_quebra_de_linha;
 }
 
+$sql = "SELECT * from livros";
 
+
+
+criaJSON(sql);
 
 
 
